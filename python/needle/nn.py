060ac9ba9bc9e371602ba44bca66aca5317aa599
Caleb_Du <Caleb_Du@zju.edu.cn>
2022-09-30 13:47:05 +0800

    complete ReLU Module in nn.py

--- python/needle/nn.py
+++ python/needle/nn.py
@@ -114,7 +114,7 @@ class Flatten(Module):
 class ReLU(Module):
     def forward(self, x: Tensor) -> Tensor:
         ### BEGIN YOUR SOLUTION
-        raise NotImplementedError()
+        return ops.relu(x)
         ### END YOUR SOLUTION
 
 
