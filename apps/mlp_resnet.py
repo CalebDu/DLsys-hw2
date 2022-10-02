30848df11c8bb0dc0f1f39a5d54fae1613d3331b
Caleb_Du <Caleb_Du@zju.edu.cn>
2022-10-02 15:53:55 +0800

    complete ResidualBlock in mlp_resnet.py

--- apps/mlp_resnet.py
+++ apps/mlp_resnet.py
@@ -1,4 +1,5 @@
 import sys
+
 sys.path.append('../python')
 import needle as ndl
 import needle.nn as nn
@@ -8,18 +9,27 @@ import os
 
 np.random.seed(0)
 
+
 def ResidualBlock(dim, hidden_dim, norm=nn.BatchNorm1d, drop_prob=0.1):
     ### BEGIN YOUR SOLUTION
-    raise NotImplementedError()
+    fn = nn.Sequential(nn.Linear(in_features=dim, out_features=hidden_dim),
+                       norm(dim=hidden_dim), nn.ReLU(), nn.Dropout(drop_prob),
+                       nn.Linear(in_features=hidden_dim, out_features=dim),
+                       norm(dim=dim))
+    block = nn.Sequential(nn.Residual(fn), nn.ReLU())
+    return block
     ### END YOUR SOLUTION
 
 
-def MLPResNet(dim, hidden_dim=100, num_blocks=3, num_classes=10, norm=nn.BatchNorm1d, drop_prob=0.1):
+def MLPResNet(dim,
+              hidden_dim=100,
+              num_blocks=3,
+              num_classes=10,
+              norm=nn.BatchNorm1d,
+              drop_prob=0.1):
     ### BEGIN YOUR SOLUTION
     raise NotImplementedError()
     ### END YOUR SOLUTION
-
-
 
 
 def epoch(dataloader, model, opt=None):
@@ -29,14 +39,17 @@ def epoch(dataloader, model, opt=None):
     ### END YOUR SOLUTION
 
 
-
-def train_mnist(batch_size=100, epochs=10, optimizer=ndl.optim.Adam,
-                lr=0.001, weight_decay=0.001, hidden_dim=100, data_dir="data"):
+def train_mnist(batch_size=100,
+                epochs=10,
+                optimizer=ndl.optim.Adam,
+                lr=0.001,
+                weight_decay=0.001,
+                hidden_dim=100,
+                data_dir="data"):
     np.random.seed(4)
     ### BEGIN YOUR SOLUTION
     raise NotImplementedError()
     ### END YOUR SOLUTION
-
 
 
 if __name__ == "__main__":
